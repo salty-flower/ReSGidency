d7315ac0ee247560b3cc88e9b3007ff8b6b90529
salty-flower <95509524+salty-flower@users.noreply.github.com>
2024-04-14 18:21:09 +0800

    feat: add examples to `InstitutionDescriptors`

--- src/ReSGidency.Models/InstitutionDescriptors.cs
+++ src/ReSGidency.Models/InstitutionDescriptors.cs
@@ -7,7 +7,7 @@ public abstract class InstitutionDescriptors
         // Singapore
 
         Autonomous,
-        NTU_NUS,
+        NTU_NUS, // eg. "新二"
 
         // Malaysia
 
@@ -18,22 +18,23 @@ public abstract class InstitutionDescriptors
         QS100,
 
         // Chinese
-        Top2,
+        Top2, // eg. "清北"
         C9,
-        MOE985,
-        MOE211,
-        Tier1,
+        MOE985, // eg. "985", "985本", "985硕士", "9本"
+        MOE211, // eg. "211本"
+        Tier1, // eg. "一本", "双非一本"
+        Tier2, // eg. "二本"
 
         // United States
         IvyLeague,
 
         // United Kingdom
         RussellGroup,
-        Oxbridge,
+        Oxbridge, // eg. "牛剑"
         GoldenTriangle,
 
         // Australia
-        GroupOfEight,
+        GroupOfEight, // eg. "八大", "澳八大"
         Sandstone,
 
         // Coming soon...
